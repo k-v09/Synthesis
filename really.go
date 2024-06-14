6cd1b155d1a2e897efe613d59c746b195b573949
Noah Sehman <noah.sehman@gmail.com>
2024-06-14 11:39:25 -0700

    WE HAVE SOUND BABY!!! Actually deafening but sound is sound

--- really.go
+++ really.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"log"
 	"math"
 	"os"
+	"strings"
 
 	"github.com/go-audio/wav"
 )
@@ -34,39 +35,59 @@ func tWave(wave Wave, waveFactor float64) (float64, float64) {
 	return nn, sample
 }
 
-func writeWav() {
-	w, err := os.Open("waves/sin.wav")
-	if err != nil {
-		panic(fmt.Sprintf("couldn't open audio file - %v", err))
-	}
+func inclusion() (*wav.Encoder, *os.File, error) {
+	w, err := os.Create("waves/wave0.wav")
 	var enc = wav.NewEncoder(w, sr, 32, 1, 1) //fmt.Printf("%b\n", math.Float64bits(52.0))
+	return enc, w, err
+}
+
+func Input() {
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Simple Shell")
+	fmt.Println("------------------------")
+
+	for {
+		fmt.Print("->")
+		text, _ := reader.ReadString('\n')
+		text = strings.Replace(text, "\n", "", -1)
+		if strings.Compare("hi", text) == 0 {
+			fmt.Println("Hiya cutie ;)")
+			break
+		}
+	}
 }
 
 func main() {
-
-	var qs, q2, q3 float64
-	var time int = 2 // where time is the duration of the sample in seconds
-	var osc1, osc2, osc3 Wave
+	var (
+		qs   float64
+		time int = 2 // where time is the duration of the sample in seconds
+		osc1 Wave
+	)
 	osc1.frequency = 440
 	osc1.amplitude = 0.5
 	osc1.angle = 0
 	osc1.offset = (2 * math.Pi * osc1.frequency) / float64(sr)
-	osc2 = osc1
-	osc3 = osc2
-	file, err := os.Create("waves/test.txt")
-
+	Input()
+	code, out, err := inclusion()
 	if err != nil {
-		log.Fatalf("failed creating file: %s", err)
-	}
-	defer file.Close()
-	if err != nil {
-		log.Fatalf("failed writing to file: %s", err)
+		panic(fmt.Sprintf("couldn't open audio file - %v", err))
 	}
 
 	for i := 0; i < sr*time; i++ {
 		osc1.angle, qs = sinWave(osc1)
-		osc2.angle, q2 = sqWave(osc2)
-		osc3.angle, q3 = tWave(osc3, 1.0)
-		file.WriteString(fmt.Sprintf("%f", qs) + " " + fmt.Sprintf("%f", q2) + " " + fmt.Sprintf("%f", q3) + "\n")
+		// osc2.angle, q2 = sqWave(osc2)
+		// osc3.angle, q3 = tWave(osc3, 1.0)
+		code.WriteFrame(float32(qs))
 	}
+	code.Close()
+	out.Close()
+	out, err = os.Open("waves/wave0.wav")
+	if err != nil {
+		panic(err)
+	}
+	d2 := wav.NewDecoder(out)
+	d2.ReadInfo()
+	fmt.Println("New file ->", d2)
+	out.Close()
 }
